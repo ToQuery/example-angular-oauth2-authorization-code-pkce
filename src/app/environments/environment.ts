9d59a0582e0800246508e45f43ba622bfc748900
ToQuery <toquery@qq.com>
2024-02-04 23:47:04 +0800

    upgrade

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -4,10 +4,10 @@
 
 export const environment = {
   production: false,
-  stsAuthority: 'https://demo.duendesoftware.com/',
-  clientId: 'interactive.public',
+  stsAuthority: 'http://example-spring-authorization-server.local:9000/',
+  clientId: 'example-angular-oauth2-authorization-code-pkce',
   clientRoot: 'http://localhost:4200/',
-  clientScope: 'openid profile email api',
+  clientScope: 'openid',
   apiRoot: 'https://demo.duendesoftware.com/api',
 };
 
